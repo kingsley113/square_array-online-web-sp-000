4c08d832e1fad7f263122f6ac64cbd0dd8e6a872
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-07-10 18:34:46 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,12 @@
 def square_array(array)
   # your code here
+square_numbers = array
+
   array.each do | number |
-    square = number * number
-    puts "#{square}"
+    square_numbers = number * number
   end
 end
 
-numbers = [1,2,3,4,5]
+#numbers = [1,2,3,4,5]
 
-square_array(numbers)
+#square_array(numbers)
